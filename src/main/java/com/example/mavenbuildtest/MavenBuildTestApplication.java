ed60596c71ad3d996b5ef50a9b7697529ff2e2b9
eunjee <rira9817@gmail.com>
2024-02-01 01:17:09 +0900

    initialize war project

---
+++ src/main/java/com/example/mavenbuildtest/MavenBuildTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenbuildtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenBuildTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenBuildTestApplication.class, args);
+	}
+
+}
